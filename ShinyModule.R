1c0672009a0388e2df5d7d3e20614f76f43eec40
tavakol5272 <148060212+tavakol5272@users.noreply.github.com>
2025-11-24 12:32:03 +0000

    new version

--- ShinyModule.R
+++ ShinyModule.R
@@ -1,101 +1,202 @@
-library('move')
-library('shiny')
-library('foreach')
-library('fields')
-library('geosphere')
-library("shinycssloaders")
+library(move2)
+library(shiny)
+library(foreach)
+library(geosphere)
+library(shinycssloaders)
+library(sf)
 
+###### Interface #####################
 
-Sys.setenv(tz="UTC")
-options(scipen=999)
-#data <- readRDS("input_lowres_geese_74X.rds")
-
-## spinner option can be modified to different shapes and colors
-## zoom in to plot ==> not finished 
-## download plot ==> adjust height/width
-
-shinyModuleUserInterface <- function(id, label) {
+shinyModuleUserInterface <- function(id, label = NULL) {
   ns <- NS(id)
   
-  tagList(
+  fluidPage(
     titlePanel("Distance to Location over Time"),
-    fluidRow(
-      column(3, numericInput(ns("posi_lon"), "Longitude of Reference Location:", value=0, min = -180, max = 180,step=0.00001)),
-      column(3, numericInput(ns("posi_lat"), "Latitude of Reference Location:", value=0, min = -90, max = 90,step=0.00001)),
-      column(2, actionButton(ns("GoButton"), label="Update!", icon=icon("rotate")),style = 'top: 25px;position:relative;'),
-      column(2, downloadButton(ns('savePlot'), 'Save Plot'),style = 'top: 25px;position:relative;'),
-      
-    ),
-    withSpinner(plotOutput(ns("timeline"), height="80vh"))
-    # withSpinner(plotOutput(ns("timeline"),dblclick = ns("plot_dblclick"), brush = brushOpts(id =ns("plot_brush"),resetOnNew = TRUE), height="80vh")), ## still have to add zoom in plot axis
     
+    sidebarLayout(
+      sidebarPanel(width = 4,
+                   
+                   h4("Animals"),
+                   checkboxGroupInput(ns("animals"), NULL, choices = NULL),
+                   fluidRow(
+                     column(6, actionButton(ns("select_all_animals"), "Select All", class = "btn-sm")),
+                     column(6, actionButton(ns("unselect_animals"), "Unselect All", class = "btn-sm"))   ),
+                   
+                   h4("Reference Location"),
+                   fluidRow(
+                     column(6, numericInput(ns("posi_lon"), "Longitude of Reference Location:", value=0, min = -180, max = 180,step=0.00001)),
+                     column(6, numericInput(ns("posi_lat"), "Latitude of Reference Location:", value=0, min = -90, max = 90,step=0.00001))  ),
+                   
+                   hr(),
+                   actionButton(ns("apply_btn"), "Apply Changes", class = "btn-primary btn-block"),
+                   hr(),
+                   
+                   h4("Download:"),
+                   downloadButton(ns("save_plot"), "Download plot", class = "btn-sm")
+      ),
+      
+      mainPanel(withSpinner(plotOutput(ns("timeline"), height = "80vh")))
+    )
   )
 }
 
+########### server ###############
 
 shinyModule <- function(input, output, session, data) {
-  # dataObj <- reactive({ data })
-  current <- reactiveVal(data)
+  ns <- session$ns
+  
+  # transfer to WGS84 if needed
+  mv <- data
+  if (!sf::st_is_longlat(mv)) {
+    mv <- sf::st_transform(mv, 4326)
+  }
+  
+  trk_col    <- mt_track_id_column(mv)
+  all_animals <- unique(as.character(mv[[trk_col]]))
+  
+  #### Select animals in side bar#################
+  observe({
+    updateCheckboxGroupInput(session = session, inputId = "animals",  choices  = all_animals, selected = all_animals)
+  })
+  
+  #select all individuals
+  observeEvent(input$select_all_animals, {
+    updateCheckboxGroupInput(session = session, inputId = "animals", selected = all_animals )
+  })
+  
+  #unselect all individuals
+  observeEvent(input$unselect_animals, {
+    updateCheckboxGroupInput( session = session, inputId = "animals", selected = character(0) )
+  })
+  
+  
+  #### Reference coordinates ######### 
   userCorrds <- reactiveValues(posi_lon = 0, posi_lat = 0)
-  observeEvent(input$GoButton, {
+  
+  observeEvent(input$apply_btn, {
     userCorrds$posi_lon <- input$posi_lon
     userCorrds$posi_lat <- input$posi_lat
   })
-  timestamp_range <- range(timestamps(data))
-  timestamp_unit30 <- units(difftime(timestamp_range[1],seq(timestamp_range[1],timestamp_range[2],len=30)[2]))
-  timestamp_labs <- round(seq(timestamp_range[1],timestamp_range[2],len=30),unit=timestamp_unit30)
-  dist_range <- reactive({
-    range(distVincentyEllipsoid(coordinates(data),c(userCorrds$posi_lon,userCorrds$posi_lat)))
+  
+  ## Filter data by selected animals
+  mv_sel <- reactive({
+    req(input$animals)
+    mv[mv[[trk_col]] %in% input$animals, ]
   })
   
-  data.split <- move::split(data)
-  namen <- namesIndiv(data)
-  cols <- tim.colors(length(namen))
   
-  dist_to_loc <- reactive({
-    foreach(datai = data.split) %do% {
-      disti <- distVincentyEllipsoid(coordinates(datai),c(userCorrds$posi_lon,userCorrds$posi_lat))
-      timei <- timestamps(datai)
-      data.frame(disti,timei)
+  ## make distance table
+  dist_table <- reactive({
+    dat <- mv_sel()
+    req(nrow(dat) > 0)
+    
+    # split
+    data_split <- split(dat, dat[[trk_col]])
+    
+    foreach(datai = data_split, .combine = rbind) %do% {
+      idi   <- rep(unique(as.character(datai[[trk_col]])), nrow(datai))
+      cooi  <- sf::st_coordinates(datai)
+      timei <- mt_time(datai)
+      disti <- geosphere::distVincentyEllipsoid(
+        cooi,
+        c(userCorrds$posi_lon, userCorrds$posi_lat)
+      )
+      
+      data.frame(
+        individual_local_identifier = idi,
+        timestamp = timei,
+        location_long = cooi[, 1],
+        location_lat = cooi[, 2],
+        distance_to_location_m = disti
+      )
     }
   })
+ 
+   ##Names
+  namen <- reactive({
+    df <- dist_table()
+    unique(as.character(df$individual_local_identifier))  })
   
+  ##Colors
+  cols <- reactive({grDevices::hcl.colors(length(namen()), palette = "Viridis")  })
+  
+  ######  Plot ########
+  sv_plot <- reactiveVal(NULL)
   output$timeline <- renderPlot({
-    par(mar=c(12,4,4,2)+0.1,lab=c(10,6,20))
-    plot(timestamp_range,dist_range(),type="l",xlim=timestamp_range,ylim=dist_range(),xlab="",ylab="distance to reference location (m)",col="white",axes=FALSE)
+     
+    df <- dist_table()
+    req(nrow(df) > 0)
+    
+    ## time axis
+    timestamp_range <- range(df$timestamp)
+    time_seq  <- seq(timestamp_range[1], timestamp_range[2], length.out = 30)
+    time_diff <- difftime(time_seq[2], time_seq[1])
+    timestamp_unit30 <- attr(time_diff, "units")
+    timestamp_labs   <- round(time_seq, units = timestamp_unit30)
+    
+    ## distance range
+    dist_range <- range(df$distance_to_location_m, na.rm = TRUE)
+    
+    ## split by individual
+    df_split <- split(df, df$individual_local_identifier)
+    indiv_names <- namen()
+    indiv_cols  <- cols()
+    
+    par(mar = c(8, 5, 4, 2) + 0.1)
+    plot(
+        x = timestamp_range,
+        y = dist_range(),
+        type = "n",   
+        xlim = timestamp_range,
+        ylim = dist_range(),
+        xlab = "",
+        ylab = "Distance to reference location (m)",
+        axes = FALSE  )
+      
+    abline(h   = pretty(dist_range(), n = 6),  col = "grey90",  lty = "dotted"  )
     box()
     axis(2)
-    axis(1,at=as.POSIXct(timestamp_labs),lab=as.character(timestamp_labs),las=2)
-    if (timestamp_unit30 %in% c("secs","mins","hours")) mtext("time", side=1, line=10)
-    if (timestamp_unit30 %in% c("days","weeks")) mtext("time",side=1, line=7)
-    for (i in seq(along=namen))
-    {
-      lines(dist_to_loc()[[i]]$timei,dist_to_loc()[[i]]$disti,col=cols[i],lwd=2)
-      legend("topright",legend,namen,fill=cols)
-    }
-    sv_plot <- recordPlot()
+    axis(side = 1, at   = as.POSIXct(timestamp_labs), lab  = as.character(timestamp_labs),las  = 2  )
+    mtext("time", side = 1, line = 6)
+    
+    title("Distance to reference location over time")
+    
+    # draw one line per individual
+    for (i in seq_along(indiv_names)) {
+      dfi <- df_split[[indiv_names[i]]]
+      lines(x = dfi$timestamp,y   = dfi$distance_to_location_m, col = indiv_cols[i], lwd = 2) }
+    
+    legend("topright",legend = indiv_names, col = indiv_cols, lwd    = 2, bty= "O")
+      
+    # store plot
+    sv_plot(recordPlot())
+    })
+    
+    
     
     ### save plot ###
-    output$savePlot <- downloadHandler(
-      filename = "distance_plot.png",
+    output$save_plot <- downloadHandler(
+      filename = function() { paste0("distance_plot_",input$posi_lon, "_", input$posi_lat, ".png" ) },
       content = function(file) {
         png(file, width = 960, height = 480)
-        replayPlot(sv_plot)
+        replayPlot(sv_plot())   
         dev.off()
       }
     )
-    ### save table ###
-    csv_out <- foreach(datai = data.split, .combine = rbind) %do% {
-      idi <- rep(namesIndiv(datai),n.locs(datai))
-      cooi <- coordinates(datai)
-      timei <- timestamps(datai)
-      disti <- distVincentyEllipsoid(coordinates(datai),c(userCorrds$posi_lon,userCorrds$posi_lat))
-      data.frame("individual_local_identifier"=idi,"timestamp"=timei,"location_long"=cooi[,1],"location_lat"=cooi[,2],"distance_to_location (m)"=disti)
-    }
-    write.csv(csv_out,file=appArtifactPath("distance_table.csv"),row.names=FALSE)
-  })
-  
-  return(reactive({ current() })) 
+    
+    
+    ## Save table (CSV) 
+    observe({
+      df <- dist_table()
+      req(nrow(df) > 0)
+      csv_out <- df
+      
+      write.csv(csv_out,  file= appArtifactPath("distance_table.csv"), row.names = FALSE  )
+    })
+    
+    return(reactive({ mv_sel() }))
 }
 
 
+    
+   
